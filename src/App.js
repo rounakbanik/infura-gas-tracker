c39c5e6cca1c3702664f9d47899d44b9b7b913f8
Rounak Banik <rounakbanik@gmail.com>
2023-02-23 17:29:39 +0530

    Add comments

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { useInterval } from './hooks/useInterval';
 // Infura URl
 const baseUrl = "https://mainnet.infura.io/v3/fbd1cd3ce9494434ac35c07bac0e4e74";
 
+// Data and configuration required to make the API request
 let data = JSON.stringify({
   "jsonrpc": "2.0",
   "method": "eth_feeHistory",
@@ -28,8 +29,11 @@ let config = {
 
 function App() {
 
+  // Variable that holds history of the last 5 blocks
   const [feeHistory, setFeeHistory] = useState(null);
 
+  // useInterval is like useEffect, except that it is called after a certain interval 
+  // In our case, this is 15 seconds
   useInterval(() => {
 
     const getFeeHistory = async () => {
@@ -51,12 +55,15 @@ function App() {
       let blockRange = []
       for (let i = 0; i < 5; i++) blockRange.push(history['oldestBlock'] + i);
 
+      // Create a 2D array consisting of all the information received from the API
       let formattedHistory = [
         blockRange,
         history['baseFeePerGas'].slice(0, 5),
         history['gasUsedRatio']
       ];
 
+      // Transpose the array
+      // This is done so we can populate HTML tables more easily
       const transpose = m => m[0].map((x, i) => m.map(x => x[i]))
       formattedHistory = transpose(formattedHistory);
 
@@ -64,6 +71,7 @@ function App() {
       console.log(formattedHistory);
     }
 
+    // Call function every 15 seconds
     getFeeHistory();
 
   }, 1000 * 15)
